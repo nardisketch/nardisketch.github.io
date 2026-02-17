970dc3f77b38640f68b81c4c28d984dbf745ee97
Ricardo Santos <rick.giometti@hotmail.com>
2026-02-17 17:54:21 -0300

    feat: add gallery images and update image references in index.html and wip: smaller displays

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ const navLinks = document.getElementById('navLinks');
 const navBar = document.getElementById('navBar');
 
 hamburger.addEventListener('click', () => {
-    console.log('Hamburger clicked');
+    hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
     navBar.classList.toggle('active');
+});
+
+console.log(navLinks);
+
+navLinks.querySelectorAll("li").forEach(link => {
+    console.log(link);
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navBar.classList.remove('active');
+        navLinks.classList.remove('active');
+    });
 });
